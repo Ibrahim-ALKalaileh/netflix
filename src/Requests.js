3df193bdaa9ad66671b4c18952fd56947620b548
Ibrahim Kalaileh <ibrahim.al.kalaileh@gmail.com>
2023-02-22 09:00:33 +0300

    added TMDB Api and basic fetch for a random movie

---
+++ src/Requests.js
@@ -0,0 +1,16 @@
+// tYPICALLY WE WOULD STORE IN {process.env.API_KEY}
+const API_KEY ="4c08ec1b5e3375c9900a88b5f7c83f46";
+
+
+const requests = {
+    fetchTrending:`/trending/all.week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals:`/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated:`/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies:`/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies:`/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies:`/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies:`/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries:`/discover/movie?api_key=${API_KEY}&with_genres=99`,
+}
+
+export default requests;
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,16 +1,51 @@
-import React from 'react'
+import React, { useEffect,useState } from 'react'
 import './Banner.css'
 import banner from '../assets/img/Banner.jpg'
+import axios from "../axios"
+import requests from '../Requests'
 
 function Banner() {
+
+    const [movie,setMovie]=useState([]);
+
+    useEffect(()=>{
+        async function fetchData(){
+            const request = await axios.get(requests.fetchNetflixOriginals)
+            setMovie(
+                request.data.results[
+                    Math.floor(Math.random()*request.data.results.length-1)
+                ]
+            )
+            return request;
+        }
+
+        fetchData();
+    },[])
+
+    function truncate(string,n){
+        return string?.length > n ? string.substr(0,n-1)+' ...' : string
+    }
+
   return (
-    <div className='banner' style={{
+    <header className='banner' style={{
         backgroundSize:"cover",
-        backgroundImage:`url(${banner})`,
+        backgroundImage:`url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
         backgroundPosition:"center center"
     }}>
-        
-    </div>
+        <div className='banner__contents'>
+            <h1 className='banner__title'>Movie Name</h1>
+            <div className='banner__buttons'>
+                <button className='banner__button'>Play</button>
+                <button className='banner__button'>My List</button>
+            </div>
+            <h1 className='banner__description'>
+                 {truncate('this is my descriptitis is my ription testis is my deion test', 50)}
+                 </h1>
+        </div>
+
+        <div className='banner--fadeBottom'/>
+
+    </header>
   )
 }
 
