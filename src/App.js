3f4dafbee0a4bf643458e74e51c4064c355b775c
Ibrahim Kalaileh <ibrahim.al.kalaileh@gmail.com>
2023-02-25 21:14:49 +0300

    modified

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
+          <Route path="/contact" element={</>} />
         </Routes>
       </Router>
     </div>
