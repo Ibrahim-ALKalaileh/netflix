51d204ce3684c8092dcfb0dc730834271cf71d8d
Ibrahim Kalaileh <ibrahim.al.kalaileh@gmail.com>
2023-03-03 16:51:41 +0300

    fixed firebase signup function

--- src/components/SignUpScreen.js
+++ src/components/SignUpScreen.js
@@ -1,15 +1,49 @@
-import React from 'react'
+import React,{useRef} from 'react'
+import { auth } from '../firebase';
+import { createUserWithEmailAndPassword } from "firebase/auth";
 import './SignUpScreen.css'
 
 function SignUpScreen() {
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+
+  const register = (e)=>{
+    e.preventDefault();
+
+    createUserWithEmailAndPassword(auth,
+      emailRef.current.value,
+      passwordRef.current.value
+      ).then(()=>{})
+      .catch((error)=>{
+        alert(error.message)
+      });
+  };
+
+  const signIn = (e)=> {
+    e.preventDefault();
+    
+    auth.signInWithEmailAndPassowrd(
+      emailRef.current.value,
+      passwordRef.current.value
+    ).then(()=>{})
+    .catch((error)=>{
+      alert(error.message)
+    });
+
+  }
+
   return (
     <div className='signupScreen'>
       <form>
         <h1>Sign In</h1>
-        <input placeholder='Email' type="Email"/>
-        <input placeholder='Password' type="password"/>
-        <button type='submit'>Sign In</button>
-        <h4></h4>
+        <input ref={emailRef} placeholder='Email' type="Email"/>
+        <input ref={passwordRef} placeholder='Password' type="password"/>
+        <button type='submit' onClick={signIn}>Sign In</button>
+
+        <h4> 
+          <span className='singupScreen__gray'>New to Netflix? </span> 
+          <span className='signupScreen_link' onClick={register}>Sign Up now.</span>
+        </h4>
       </form>
     </div>
   )
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,8 @@
+
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from "firebase/firestore";
+
 const firebaseConfig = {
   apiKey: "AIzaSyAZSEUJgFP22y4uj9OK81ZGpReVfxPHRNU",
   authDomain: "netflix-52711.firebaseapp.com",
@@ -6,3 +11,13 @@ const firebaseConfig = {
   messagingSenderId: "5208647359",
   appId: "1:5208647359:web:bbd89b05d020054776880a"
 };
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const db = getFirestore(firebaseApp)
+
+const auth = getAuth(firebaseApp)
+
+export {auth};
+
+export default db;
