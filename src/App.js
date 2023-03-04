95d2e2f68aad108ed37ebc21cce8a20f382ebda8
Ibrahim Kalaileh <ibrahim.al.kalaileh@gmail.com>
2023-03-05 00:02:19 +0300

    implimented login and logout using redux

--- src/App.js
+++ src/App.js
@@ -4,28 +4,43 @@ import LoginScreen from './components/LoginScreen'
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { auth } from './firebase';
+import { useDispatch, useSelector } from 'react-redux';
+import { login, logout, selectUser } from './features/user/userSlice';
 
 function App() {
-  const user = null;
-
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+   
   useEffect(()=>{
-    auth.onAuthStateChanged(userAuth=>{
+
+    const unsubscribe = auth.onAuthStateChanged(userAuth=>{
       if (userAuth){
         //logged in
+        dispatch(login({
+          uid: userAuth.uid,
+          email: userAuth.email
+        }))
       } else {
         //logged out
+        dispatch(logout)
       }
     })
+    // detatch the onAuthStateChanged Listener
+    return unsubscribe;
   },[])
 
   return (
     <div className="app">
       
       <Router>
-        {!user ? (<LoginScreen/>):
+        {!user ? (
+        <LoginScreen/>
+        ): (
         <Routes>
+        <Route exact path="/profile" element={<HomeScreen/>} />
         <Route exact path="/" element={<HomeScreen/>} />
-      </Routes>}
+        </Routes>
+      )}
         
       </Router>
     </div>
